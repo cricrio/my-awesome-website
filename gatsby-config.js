ef234439b8313a795e1c6f7b636c53c5be8d7c8a
Christopher Bellanger <cricrio@users.noreply.github.com>
2019-07-09 14:11:11 +0200

    Update metadata and add gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Christopher Bellanger Awesome Web Site`,
+    description: `Christopher Bellanger: Human Maker and Comedian`,
+    author: `Christopher Bellanger`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +18,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Christopher Bellanger Awesome Web Site`,
+        short_name: `cbws`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: ``, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
   ],
 }
