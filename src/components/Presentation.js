2e8078eba89db5578c0b3251d622c5794fd51502
Christopher Bellanger <cricrio@users.noreply.github.com>
2019-07-09 16:48:25 +0200

    Move presentation in component

---
+++ src/components/Presentation.js
@@ -0,0 +1,50 @@
+import React from "react"
+import styled from "styled-components"
+import { Carousel } from "./Carousel"
+import { AwesomeText } from "./AwesomeText"
+import { SocialLink } from "./SocialLink"
+
+export const Presentation = () => (
+  <Container className="presentation">
+    <Profil className="profil">
+      <Name className="name">Christopher Bellanger</Name>
+      <Carousel>
+        <AwesomeText text={"Human"} />
+        <AwesomeText text={"Maker"} />
+        <AwesomeText text={"Comedian"} />
+      </Carousel>
+      <SocialLinks className="social-links">
+        <SocialLink href="https://www.linkedin.com/in/christopher-bellanger-074817117/">
+          LinkedIn
+        </SocialLink>
+        <SocialLink href="https://github.com/cricrio">Github</SocialLink>
+        <SocialLink href="https://twitter.com/_cricrio_">Twitter</SocialLink>
+      </SocialLinks>
+    </Profil>
+  </Container>
+)
+
+const Container = styled.div`
+  font-family: sans-serif;
+  height: 100vh;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Profil = styled.section`
+  color: white;
+  height: 250px;
+`
+
+const Name = styled.h1`
+  font-size: 3rem;
+  justify-content: space-between;
+`
+
+const SocialLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  font-size: 1.5rem;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,8 @@
 import React from "react"
 import "./index.css"
 
-import { AwesomeText } from "../components/AwesomeText"
-import { Carousel } from "../components/Carousel"
-import { SocialLink } from "../components/SocialLink"
+import { Presentation } from "../components/Presentation"
 
-const IndexPage = () => (
-  <div className="outer">
-    <div className="inner">
-      <div className="profil">
-        <h1 className="name">Christopher Bellanger</h1>
-        <Carousel>
-          <AwesomeText text={"Human"} />
-          <AwesomeText text={"Maker"} />
-          <AwesomeText text={"Comedian"} />
-        </Carousel>
-        <div className="social-links">
-          <SocialLink href="https://www.linkedin.com/in/christopher-bellanger-074817117/">
-            LinkedIn
-          </SocialLink>
-          <SocialLink href="https://github.com/cricrio">Github</SocialLink>
-          <SocialLink href="https://twitter.com/_cricrio_">Twitter</SocialLink>
-        </div>
-      </div>
-    </div>
-  </div>
-)
+const IndexPage = () => <Presentation />
 
 export default IndexPage
